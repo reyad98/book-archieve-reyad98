b7b2273c376928d53844563e5690fa45d17c9912
reyad98 <alexmarfi2233@gmail.com>
2021-09-02 16:36:45 +0600

    we

--- book.js
+++ book.js
@@ -1,3 +1,4 @@
+
 const searchBook = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
@@ -6,53 +7,49 @@ const searchBook = () => {
 
   const url = ` https://openlibrary.org/search.json?q=${searchText}
     `;
-  // console.log(url);
-   
+//  for find all search result
   fetch(url)
     .then(res => res.json())
-    //.then(data =>displaySearchResult(data.docs))
-    .then(data =>displayFound(data.numFound));
-    
-    fetch(url)
-    .then(res => res.json())
-    .then(data =>displaySearchResult(data.docs))
-    //.then(data =>console.log(data.numFound));
-    
-    
-}
+    .then(data => displayFound(data.numFound));
 
-  const displayFound = total =>{
-     if(total == 0)
-     {
-      const result = document.getElementById('error');
-      result.textContent='';
-      const div = document.createElement('div');
-      div.innerHTML=`
+//  for  show some book result   
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.docs))
+
+}
+// arrow function for found all search result
+const displayFound = total => {
+  if (total == 0) {
+    const result = document.getElementById('error');
+    result.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
       <h4 class="text-center bgcol w-50 mx-auto p-2 rounded">
       Not Found</h4>
       `;
-      result.appendChild(div);
-     }
-    
-     else
-     {
-      const result = document.getElementById('error');
-      result.textContent='';
-      const div = document.createElement('div');
-      div.innerHTML=`
+    result.appendChild(div);
+  }
+
+  else {
+    const result = document.getElementById('error');
+    result.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
       <h4 class="text-center bgcol w-50 mx-auto p-2 rounded">
       Found Result:${total}</h4>
       `;
-      result.appendChild(div);
-     }
-   //console.log(total);
+    result.appendChild(div);
   }
-    
+  
+}
 
-  const displaySearchResult = books => {
+// arrow function for display some book result
+const displaySearchResult = books => {
   const searchResult = document.getElementById('search-result');
 
-  searchResult.textContent='';
+  searchResult.textContent = '';
+  
   books.forEach(book => {
     //const pp =book.numFound;
     const div = document.createElement('div');
@@ -63,14 +60,14 @@ const searchBook = () => {
         <div class="card-body">
           <h4 class="card-title">Title: ${book.title}</h4>
           <h5 class="card-title">Author: ${book.author_name}</h5>
-          <h6 class="card-title">Publisher: ${book.publisher[0]}</h6>
+          <h6 class="card-title">Publisher: ${book.publisher}</h6>
           <p class="card-text">Published Date: ${book.first_publish_year}</p>
           
         </div>
       </div>
         `;
     searchResult.appendChild(div);
-    
+
   })
 }
 
